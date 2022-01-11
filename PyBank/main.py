826f1bbd177c56b9ed533e28c9d8eb2adbc0d04b
Catharine Romero <catharineromero@gmail.com>
2022-01-11 01:10:02 -0500

    Adding analysis text//almost done

--- PyBank/main.py
+++ PyBank/main.py
@@ -19,16 +19,13 @@ with open(py_bank) as csvfile:
         for row in csvreader:
             months.append(row[0])
             profloss.append(row[1])
-        
+
         print("Financial Analysis")
         print("----------------------------")
 
-        #Defining a function in order to find the length of the list
-        def bank_month(month_length):
-            #Print total months
-            print("Total Months: " + str(len(month_length)))
-        #Call bank_month function
-        bank_month(months)
+        #use len to find the total months
+        total_months = len(months)
+        print("Total Months: " + str(total_months))
 
         #list comprhension turning string into integer
         profloss = [int (profloss_values) for profloss_values in profloss]
@@ -50,16 +47,30 @@ with open(py_bank) as csvfile:
             for number in numbers:
                 total += number
             return total / length
+
         #Call average function    
         average(profloss_diff)
         print("Average Change: $" + str((round(average(profloss_diff),2))))
 
         greatest_increase = max(profloss_diff)
         greatest_decrease = min(profloss_diff)
-        print(greatest_increase) 
-        print(greatest_decrease)
 
-        #print(months)
-        #print(profloss_diff)
+        x = profloss_diff.index(greatest_increase)
+     
+        y = profloss_diff.index(greatest_decrease)
 
-        #if greatest_increase
+        print(f"Greatest Increase In Profits: {months[x+1]} (${greatest_increase})")
+        print(f"Greatest Decrease In Profits: {months[y+1]} (${greatest_decrease})")
+
+py_bank = os.path.join("Resources", "bank_analysis.txt")
+
+with open(py_bank, "w") as datafile:
+        writer = csv.writer(datafile)
+
+        writer.writerow([("Financial Analysis")])
+        writer.writerow(["----------------------------"])
+        writer.writerow(["Total Months: " + str(total_months)])
+        #writer.writerow([("Total: $" + str(total))]
+        writer.writerow([("Average Change: $" + str((round(average(profloss_diff),2))))])
+        #writer.writerow([(f"Greatest Increase In Profits: {months[x+1]} (${greatest_increase})")])
+        #writer.writerow([(f"Greatest Decrease In Profits: {months[y+1]} (${greatest_decrease})")])
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,7 +6,6 @@ py_poll = os.path.join ('Resources', 'election_data.csv')
 voter_id = []
 county = []
 canidate = []
-khan_votes = []
 
 with open(py_poll) as csvfile:
         csvreader = csv.reader(csvfile, delimiter=',')
@@ -20,13 +19,85 @@ with open(py_poll) as csvfile:
             voter_id.append(row[0])
             county.append(row[1])
             canidate.append(row[2])
-        
-        def poll_votes(total_votes):
-            print("Total Votes: " + str(len(total_votes)))
-            print(" -------------------------")
-        poll_votes(voter_id)
 
-        for canidates in canidate:
-            if canidates == Khan:
-                khan_votes.append 
-                print(len(khan_votes))  
+        total_votes = len(voter_id)
+        print("Total Votes: " + str(len(voter_id)))
+
+        canidate_set = sorted(set(canidate))
+        canidate_list = list(canidate_set)
+
+        khan_votes = 0
+        correy_votes = 0
+        li_votes = 0
+        otooley_votes = 0
+
+        for i in range(0,len(canidate)):
+            if canidate [i] == "Khan":
+                khan_votes += 1
+            elif canidate [i] == "Correy":
+                correy_votes += 1
+            elif canidate [i] == "Li":
+                li_votes += 1
+            else:
+                canidate [i] == "O'Tooley"
+                otooley_votes += 1
+        
+        khan_percent = (khan_votes/total_votes) * 100
+        correy_percent = (correy_votes/total_votes) * 100
+        li_percent = (li_votes/total_votes) * 100
+        otooley_percent = (otooley_votes/total_votes) * 100
+
+        winner = max(canidate_list, key = canidate.count)
+
+        print('Election Results')
+        print('-------------------------')
+        print("Total Votes: " + str(len(voter_id)))
+        print('------------------------')
+        print(f"{canidate_list[1]}: {khan_percent: .3f}% ({khan_votes})")
+        print(f"{canidate_list[0]}: {correy_percent: .3f}% ({correy_votes})")
+        print(f"{canidate_list[2]}: {li_percent: .3f}% ({li_votes})")
+        print(f"{canidate_list[3]}: {otooley_percent: .3f}% ({otooley_votes})")
+        print('-------------------------')
+        print(f"Winner: {winner}")
+
+
+        # ('Election Results')
+        # ('-------------------------')
+        # ("Total Votes: " + str(len(voter_id)))
+        # ('------------------------')
+        # (f"{canidate_list[1]}: {khan_percent: .3f}% ({khan_votes})")
+        # (f"{canidate_list[0]}: {correy_percent: .3f}% ({correy_votes})")
+        # (f"{canidate_list[2]}: {li_percent: .3f}% ({li_votes})")
+        # (f"{canidate_list[3]}: {otooley_percent: .3f}% ({otooley_votes})")
+        # ('-------------------------')
+        # (f"Winner: {winner}")
+
+py_poll = os.path.join("Resources", "election_results.txt")
+
+with open(py_poll, "w") as datafile:
+        writer = csv.writer(datafile)
+
+        writer.writerow(['Election Results'])
+        writer.writerow(['----------------------------'])
+        # writer.writerow(["Total Votes: " + str(len(voter_id))])
+        # writer.writerow([f"{canidate_list[1]}: {khan_percent: .3f}% ({khan_votes})")]
+        # writer.writerow([f"{canidate_list[0]}: {correy_percent: .3f}% ({correy_votes})"])
+        # writer.writerow([f"{canidate_list[2]}: {li_percent: .3f}% ({li_votes})"])
+        # writer.writerow([f"{canidate_list[3]}: {otooley_percent: .3f}% ({otooley_votes})"])
+        # writer.write.row([f"{canidate_list[3]}: {otooley_percent: .3f}% ({otooley_votes})"])
+        # writer.write.row(['-------------------------'])
+        # writer.wrtie.row([f"Winner: {winner}"])
+                # for canidates in range(1,len(canidate)):
+        #     if canidates == "Khan":
+        #         print(canidates)
+
+    # def polling_results(polling_data)
+
+    #     voter_id = (polling_data[0])
+    #     county = (polling_data[1])
+    #     canidate = (polling_data[2])
+
+    #     total_votes = len(voter_id)
+
+    #     print("Total Votes: " + str(len(voter_id))
+    
